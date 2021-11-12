09a9620bd5e58b5e343b3a53a61cf1f65ebf2317
Jingu-web <multiNomaFeBe.rjrrr@gmail.com>
2021-11-13 02:12:57 +0900

    Add: postsのルートを追加

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root 'games#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :posts
+  root to: 'games#index'
 end
